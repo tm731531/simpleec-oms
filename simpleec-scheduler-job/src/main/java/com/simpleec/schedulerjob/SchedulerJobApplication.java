9770c96adfba747eadedb56d95f7797bcbfa3249
tom <tm731531@gmail.com>
2026-02-21 14:40:36 +0800

    WIP: Task 3 HeartbeatTask implementation - debugging classpath issues

--- simpleec-scheduler-job/src/main/java/com/simpleec/schedulerjob/SchedulerJobApplication.java
+++ simpleec-scheduler-job/src/main/java/com/simpleec/schedulerjob/SchedulerJobApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = "com.simpleec")
+@SpringBootApplication(scanBasePackages = {"com.simpleec.common", "com.simpleec.schedulerjob"})
 @EnableScheduling
 public class SchedulerJobApplication {
     public static void main(String[] args) {
